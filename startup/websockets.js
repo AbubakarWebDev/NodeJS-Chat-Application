a5d66646c01ad38db8f07bed9be916e98991366e
Muhammad Abubakar <90tricks90@gmail.com>
2023-07-14 10:36:15 +0500

    websockets implementation

--- startup/websockets.js
+++ startup/websockets.js
@@ -6,11 +6,39 @@ module.exports = function (server) {
         },
     });
 
+    const onlineUsers = {};
+
     io.on('connection', (socket) => {
-        console.log('a user connected');
+        const socketId = socket.id;
+        console.log(`A user with the id: ${socketId} is connected`);
+
+        socket.on('setup', (userId) => {
+            socket.join(userId);
+
+            onlineUsers[userId] = socketId;
+
+            io.emit('onlineUsers', onlineUsers);
+        });
+
+        socket.on('sendMessage', (message) => {
+            message.chat.users.forEach((user) => {
+                if (user !== message.sender._id) {
+                    io.to(user).emit('receiveMessage', message);
+                }
+            })
+        });
 
         socket.on('disconnect', () => {
-            console.log('user disconnected');
+            for (const key in onlineUsers) {
+                if (onlineUsers[key] === socketId) { 
+                    console.log(`A user with the id: ${socketId} is disconnected`);
+
+                    socket.leave(key);
+                    delete onlineUsers[key];
+                }
+            }
+            
+            io.emit('onlineUsers', onlineUsers);
         });
     });
 }
